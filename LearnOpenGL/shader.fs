c5a5b9b7ea58ddb98f7de844f19c493cedad5b57
Brendan Parmer <bjparmer@sbcglobal.net>
2021-05-17 00:23:04 -0500

    Implemented simple texture stuff

--- LearnOpenGL/shader.fs
+++ LearnOpenGL/shader.fs
@@ -1,7 +1,11 @@
 #version 330 core //Simple C Fragment Shader Program
 	out vec4 FragColor;
 	in vec3 ourColor;
+	in vec2 ourTex;
+
+	uniform sampler2D ourTexture;
+
 	void main()
 	{
-		FragColor =	vec4(ourColor, 1.0); //right now this is a constant color
+		FragColor =	texture(ourTexture, ourTex); //right now this is a constant color
 	};
